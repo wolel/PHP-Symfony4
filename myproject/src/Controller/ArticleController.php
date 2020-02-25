24d96dd86f382c0959b5e30a1248adc8ac2e20f4
wolel <56022602+wolel@users.noreply.github.com>
2020-02-25 15:50:20 +0100

    symfony

--- myproject/src/Controller/ArticleController.php
+++ myproject/src/Controller/ArticleController.php
@@ -4,28 +4,36 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
-{
-    /**
-     * @Route("/")
-     */
+class ArticleController extends AbstractController{
+        /**
+         * @Route("/")
+         */
 
-//========================================
+    //========================================
 
-public function homepage()
-{
-    return new Response('TEST');
-}
-//========================================
-    /**
-     * @Route("/articles/{titre}")
-    */
-public function show($titre)
-{
-    return new Response("Mon article ayant pour titre".' '.$titre.' '."s'affiche");
-}
+    public function homepage()
+    {
+        return new Response('TEST');
+    }
+    //========================================
+        /**
+         * @Route("/articles/{titre}")
+        */
+    public function show($titre)
+    {
+                //chemin vers un fichier tamplet 'twig'
+                //new Response("Mon article ayant pour titre".' '.$titre.' '."s'affiche");
+        $comments = ['comm 1', 'comm 2', 'comm 3','comm 4'];
+        return $this->render('article/show.html.twig',[
+            "titre"=>$titre,
+            "maVariable"=>"TEST",
+            "comments"=>$comments
 
-}
+        ]);
+    }
+
+    }
