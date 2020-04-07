831f7fd8ab9d900bcd97d9f827c1bf61fdf65e1c
wolel <56022602+wolel@users.noreply.github.com>
2020-04-07 13:06:53 +0200

    symfony 4- fini

--- myproject/src/Controller/ArticleController.php
+++ myproject/src/Controller/ArticleController.php
@@ -7,21 +7,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
 
 class ArticleController extends AbstractController{
         /**
-         * @Route("/home")
+         * @Route("/home", name="app_homepage")
          */
 
     //========================================
 
     public function homepage()
     {
-        return new Response('TEST');
+        return $this->render('article/home.html.twig');
     }
     //========================================
         /**
-         * @Route("/articles/{titre}")
+         * @Route("/articles/{titre}", name="article_show")
         */
     public function show($titre)
     {
@@ -34,6 +35,39 @@ class ArticleController extends AbstractController{
             "comments"=>$comments
 
         ]);
+    }
+
+    /**
+     * @Route("/articleCreate", name="article_create")
+     */
+    public function create()
+    {
+       $entityManager = $this->getDoctrine()->getManager();
+       $article = new Article();
+       $article->setTitre('Titre article');
+       $article->setContenu('Contenu de mon article');
+
+       $entityManager->persist($article);//on veut gardee en memeoir notre article
+
+        $entityManager->flush();//avec cette commende, elle sera écrite dans la base de donnée
+
+        return new Response('Article a été bien sauvegardé id = '.$article->getId());
+    }
+
+
+    /**
+     * @Route("/article/{id}", name="article_show_from_name_db")
+     */
+    public function showfromDB(Article $article)
+    {
+        $comments = ['comm 1', 'comm 2', 'comm 3','comm 4'];
+
+        return $this->render('article/show.html.twig',[
+            "titre"=>$article->getTitre(),
+            "Contenu"=>$article->getContenu(),
+            "comments"=>$comments
+            ]);
+
     }
 
     }
