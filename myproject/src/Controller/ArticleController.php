a641db5ae8ac17720706aaabc7398c3d85616a5d
wolel <56022602+wolel@users.noreply.github.com>
2020-04-07 09:30:50 +0200

    symfony 4

--- myproject/src/Controller/ArticleController.php
+++ myproject/src/Controller/ArticleController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController{
         /**
-         * @Route("/")
+         * @Route("/home")
          */
 
     //========================================
